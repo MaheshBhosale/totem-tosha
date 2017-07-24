f2b2d44c827f3c3b1887fbacc38ab510287222ee
avipl <avipl1994@gmail.com>
2017-07-24 08:45:30 +0530

    response speech was not given and corrected a typo

--- app.py
+++ app.py
@@ -60,8 +60,8 @@ def process(req):
     root = ET.fromstring(req.content)
     speech = "tracking information for AWB no:" + (root[1][0].text) + "\n"
     if root[1][1][0].text != 'success':
-        specch += "No shipment data found\nPlease check your AWB number and try again"
-        return
+        speech += "No shipment data found\nPlease check your AWB number and try again"
+        return speech
     speech += "Shippment information:\n"
     speech += "Shipment started at " + root[1][2][0][0].text +" " + root[1][2][0][1].text + "\n"
     speech += "Shipment ended at " + root[1][2][1][0].text +" " + root[1][2][1][1].text + "\n"
